5ef6694086928d87306c5a1baac010d032f192cd
AdityaTiwari-Tech <tiwariaditya15092005@gmail.com>
2025-08-04 14:13:08 +0530

    newUpdate

--- pages/api/contact-entries.ts
+++ pages/api/contact-entries.ts
@@ -4,7 +4,18 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
+    const { name, email, subject, message } = req.body;
+    try {
+      const entry = await prisma.contact.create({
+        data: { name, email, subject, message },
+      });
+      res.status(200).json({ success: true, entry });
+    } catch (error) {
+      const errorMessage = typeof error === 'object' && error !== null && 'message' in error ? (error as { message: string }).message : 'Unknown error';
+      res.status(500).json({ success: false, error: errorMessage });
+    }
+  } else if (req.method === 'GET') {
     try {
       const entries = await prisma.contact.findMany({
         orderBy: { createdAt: 'desc' }
